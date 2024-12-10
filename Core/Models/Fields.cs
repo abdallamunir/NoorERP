6f3c4362c02af26cea0dc2edf5b8c71d722ead21
Abdalla Munir <59817674+abdallamunir@users.noreply.github.com>
2024-12-10 14:52:49 +0200

    Restructured the project

--- Core/Models/Fields.cs
+++ Core/Models/Fields.cs
@@ -1,4 +1,31 @@
-﻿namespace NoorERP.Core.Models
-{
+﻿using System.Text.Json.Serialization;
 
+namespace NoorERP.Core.Models
+{
+    public class AutoNumberField : BaseField
+    {
+        [JsonPropertyName("field_type")]
+        public override FieldTypes FieldType => FieldTypes.AutoNumber;
+
+        public override object? GetDefaultValue()
+        {
+            return 0;
+        }
+
+        [JsonPropertyName("starting_number")]
+        public int StartingNumber { get; set; } = 0;
+
+
+    }
+
+    public class GuidField : BaseField
+    {
+        [JsonPropertyName("field_type")]
+        public override FieldTypes FieldType => FieldTypes.Guid;
+
+        public override object? GetDefaultValue()
+        {
+            return Guid.Empty;
+        }
+    }
 }
---
+++ Core/Models/ModuleRegistry.cs
@@ -0,0 +1,21 @@
+﻿namespace NoorERP.Core.Models
+{
+    public class ModuleRegistry
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public List<string> Dependencies { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        // Optional properties:
+        public string Category { get; set; }
+        public string IconPath { get; set; }
+        public object ConfigurationSettings { get; set; }
+        public int InitializationOrder { get; set; }
+        public int ShutdownOrder { get; set; }
+    }
+}
+
